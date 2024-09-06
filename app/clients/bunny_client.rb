5547c4f817fc5a3636052844c3adbfd56d51d3c9
stevenpy <stevenphilbert@gmail.com>
2024-09-06 18:57:11 +0200

    Add Bunny client to create a wrapper

---
+++ app/clients/bunny_client.rb
@@ -0,0 +1,43 @@
+require 'net/http'
+
+class BunnyClient
+  BASE_URI = "https://video.bunnycdn.com"
+
+  def initialize(library_id:, access_key:)
+    @library_id = library_id
+    @access_key = access_key
+  end
+
+  def get(path)
+    http_request Net::HTTP::Get, "/library/#{@library_id}#{path}"
+  end
+
+  def post(path, body:)
+    http_request Net::HTTP::Post, "/library/#{@library_id}#{path}", body: body
+  end
+
+  private
+
+  def http_request(method, path, body: nil)
+    uri = URI("#{BASE_URI}/#{path}")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.is_a?(URI::HTTPS)
+
+    request = method.new(uri)
+    request["accept"] = "application/json"
+    request["AccessKey"] = @access_key
+
+    if request.is_a? Net::HTTP::Post
+      request.body = body.to_json
+      request["content-type"] = "application/json"
+    end
+
+    response = http.request(request)
+    if response.is_a? Net::HTTPSuccess
+      JSON.parse(response.body)
+    else
+      raise StandardError, "Request failed"
+    end
+  end
+end
