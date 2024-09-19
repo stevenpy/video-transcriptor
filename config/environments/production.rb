bfd9653e52d21890ec47e422f7a91b2ad8794bec
stevenpy <stevenphilbert@gmail.com>
2024-09-19 19:09:25 +0200

    Config solid queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
-
   # config.active_job.queue_name_prefix = "video_transcriptor_production"
 
   config.action_mailer.perform_caching = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   resources :videos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
